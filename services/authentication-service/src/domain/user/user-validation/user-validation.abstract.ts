419c4d8cc905a9478aac16ddcba1fe7bb5f79587
Jesús Ángel Rodríguez Martínez <sudo.von.contact@gmail.com>
2024-08-10 18:34:27 -0600

    feature: add validation methods to abstract class

--- services/authentication-service/src/domain/user/user-validation/user-validation.abstract.ts
+++ services/authentication-service/src/domain/user/user-validation/user-validation.abstract.ts
@@ -4,7 +4,15 @@ import { InvalidUserMethodNotImplementedYetError } from '@domain/user/user-error
 export abstract class AbstractUserValidation {
   constructor(protected userValidationRules: IUserValidationRules) {}
 
-  public validateName(name: string): void {
+  public validateName(_name: string): void {
+    throw new InvalidUserMethodNotImplementedYetError();
+  }
+
+  public validateUsername(_username: string): void {
+    throw new InvalidUserMethodNotImplementedYetError();
+  }
+
+  public validatePassword(_password: string): void {
     throw new InvalidUserMethodNotImplementedYetError();
   }
 }
