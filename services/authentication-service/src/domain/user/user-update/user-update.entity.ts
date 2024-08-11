480d5177d7674b892404daca2532395512b309fa
Jesús Ángel Rodríguez Martínez <sudo.von.contact@gmail.com>
2024-08-10 18:47:45 -0600

    chore: remove unnecessary code

--- services/authentication-service/src/domain/user/user-update/user-update.entity.ts
+++
@@ -1,8 +0,0 @@
-import { IUserUpdate } from '@domain/user/user-update/user-update.interface';
-
-export class UserUpdate implements IUserUpdate {
-  constructor(
-    public name?: string,
-    public username?: string
-  ) {}
-}
