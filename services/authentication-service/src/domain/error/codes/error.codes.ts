7c62407a3a2a8cb1d6f0ecad3c50db8abdfebdc0
Jesús Ángel Rodríguez Martínez <sudo.von.contact@gmail.com>
2024-08-17 04:28:43 -0600

    feat: add error codes

--- services/authentication-service/src/domain/error/codes/error.codes.ts
+++ services/authentication-service/src/domain/error/codes/error.codes.ts
@@ -1,3 +1,4 @@
 import { UserErrorCodes } from '@domain/error/codes/user.error.codes';
+import { UserDetailsErrorCodes } from '@domain/error/codes/user-details.error.codes';
 
-export type ErrorCodes = UserErrorCodes;
+export type ErrorCodes = UserErrorCodes | UserDetailsErrorCodes;
