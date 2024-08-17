56d9e4ca93d0b4b8426bae310c4e664a732ad347
Jesús Ángel Rodríguez Martínez <sudo.von.contact@gmail.com>
2024-08-17 04:30:20 -0600

    feat: add errors

---
+++ services/authentication-service/src/domain/user-details/errors/invalid-user-details-hobby-length.error.ts
@@ -0,0 +1,11 @@
+import { Error } from '@domain/error/entities/error.entity';
+import { userDetailsHobbyRule } from '@domain/user-details/rules/user-details.hobby.rule';
+
+export class InvalidUserDetailsHobbyLength extends Error {
+  constructor() {
+    super({
+      code: 'INVALID_USER_DETAILS_HOBBY_LENGTH',
+      message: `Please provide a hobby that consists of ${userDetailsHobbyRule.LENGTH.MIN} to ${userDetailsHobbyRule.LENGTH.MAX} characters.`
+    });
+  }
+}
---
+++ services/authentication-service/src/domain/user-details/errors/invalid-user-details-occupation-length.error.ts.ts
@@ -0,0 +1,11 @@
+import { Error } from '@domain/error/entities/error.entity';
+import { userDetailsOccupationRule } from '@domain/user-details/rules/user-details.occupation.rule';
+
+export class InvalidUserDetailsOccupationLength extends Error {
+  constructor() {
+    super({
+      code: 'INVALID_USER_DETAILS_OCCUPATION_LENGTH',
+      message: `Please provide an occupation that consists of ${userDetailsOccupationRule.LENGTH.MIN} to ${userDetailsOccupationRule.LENGTH.MAX} characters.`
+    });
+  }
+}
