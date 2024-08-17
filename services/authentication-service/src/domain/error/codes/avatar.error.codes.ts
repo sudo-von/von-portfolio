5502ac02ca995d36047e8c7f06b33a6f056ecae9
Jesús Ángel Rodríguez Martínez <sudo.von.contact@gmail.com>
2024-08-17 15:36:54 -0600

    feat: add error codes

---
+++ services/authentication-service/src/domain/error/codes/avatar.error.codes.ts
@@ -0,0 +1,1 @@
+export type AvatarErrorCodes = 'INVALID_AVATAR_MIME_TYPE' | 'INVALID_AVATAR_SIZE';
--- services/authentication-service/src/domain/error/codes/error.codes.ts
+++ services/authentication-service/src/domain/error/codes/error.codes.ts
@@ -1,4 +1,5 @@
+import { AvatarErrorCodes } from '@domain/error/codes/avatar.error.codes';
 import { UserErrorCodes } from '@domain/error/codes/user.error.codes';
 import { UserDetailsErrorCodes } from '@domain/error/codes/user-details.error.codes';
 
-export type ErrorCodes = UserErrorCodes | UserDetailsErrorCodes;
+export type ErrorCodes = AvatarErrorCodes | UserErrorCodes | UserDetailsErrorCodes;
