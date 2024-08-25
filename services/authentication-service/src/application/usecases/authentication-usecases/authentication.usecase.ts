f9d0df67b5a1d24dc7e91da36e6c374c0af47831
Jesús Ángel Rodríguez Martínez <sudo.von.contact@gmail.com>
2024-08-24 18:16:22 -0600

    feat: add usecase

--- services/authentication-service/src/application/usecases/authentication-usecases/authentication.usecase.ts
+++ services/authentication-service/src/application/usecases/authentication-usecases/authentication.usecase.ts
@@ -1,51 +1,59 @@
 import { AbstractAuthenticationUsecase } from '@domain/usecases/authentication-usecases/authentication.usecase';
+import { detailedUserToDetailedSecureUser } from '@domain/mappers/detailed-user-to-detailed-secure-user.mapper';
+import { DetailedUserUsernameAlreadyInUseError } from '@domain/errors/detailed-user/detailed-user-username-already-in-use.error';
 import { IDetailedSecureUser } from '@domain/entities/detailed-secure-user-entities/detailed-secure-user.entity';
 import { IDetailedUserRepository } from '@domain/repositories/detailed-user-repositories/detailed-user.repository';
 import { IPasswordService } from '@domain/services/password-services/password.service';
 import { IUserCreation } from '@domain/entities/user-entities/user-creation.entity';
 import { IUserCredentials } from '@domain/entities/user-credentials-entities/user-credentials.entity';
+import { IUserValidationService } from '@domain/services/user-validation-services/user-validation.service';
+import { UserCredentialsInvalidCredentialsError } from '@domain/errors/user-credentials/user-credentials-invalid-credentials.error';
 
-class AuthenticationUsecaseApplication extends AbstractAuthenticationUsecase {
+class AuthenticationUsecase extends AbstractAuthenticationUsecase {
   constructor(
     protected readonly detailedUserRepository: IDetailedUserRepository,
-    protected readonly passwordService: IPasswordService
+    protected readonly passwordService: IPasswordService,
+    protected readonly userValidationService: IUserValidationService
   ) {
-    super(detailedUserRepository, passwordService);
+    super(detailedUserRepository, passwordService, userValidationService);
   }
 
   signup = async (userCreation: IUserCreation): Promise<IDetailedSecureUser> => {
-    validateUserCreation(userCreation);
+    this.userValidationService.validateName(userCreation.name);
+    this.userValidationService.validatePassword(userCreation.password);
+    this.userValidationService.validateUsername(userCreation.username);
 
-    const user = await this.userRepository.getUser();
-    if (user) throw SingleUserOnlyError;
+    const existingDetailedUser = await this.detailedUserRepository.getDetailedUser(userCreation.username);
+    if (existingDetailedUser) throw new DetailedUserUsernameAlreadyInUseError();
 
     const hashedPassword = await this.passwordService.hashPassword(userCreation.password);
 
-    const createdUser = await this.userRepository.createUser({
+    const createdDetailedUser = await this.detailedUserRepository.createDetailedUser({
       name: userCreation.name,
-      email: userCreation.email,
       username: userCreation.username,
-      password: hashedPassword,
-      socialNetworks: []
+      password: hashedPassword
     });
 
-    const secureUser = detailedToSecureUser(createdUser);
-    return secureUser;
+    const detailedSecureUser = detailedUserToDetailedSecureUser(createdDetailedUser);
+    return detailedSecureUser;
   };
 
-  signin = async (userCredentials: IUserCredentials): Promise<DetailedSecureUser> => {
-    const user = await this.userRepository.getUser();
-    if (!user) throw InvalidCredentialsError;
+  signin = async (userCredentials: IUserCredentials): Promise<IDetailedSecureUser> => {
+    this.userValidationService.validateUsername(userCredentials.username);
+    this.userValidationService.validatePassword(userCredentials.password);
+
+    const detailedUser = await this.detailedUserRepository.getDetailedUser(userCredentials.username);
+    if (!detailedUser) throw new UserCredentialsInvalidCredentialsError();
 
     const areCredentialsValid = await this.passwordService.comparePasswords(
       userCredentials.password,
-      user.password
+      detailedUser.password
     );
-    if (!areCredentialsValid || userCredentials.email !== user.email) throw InvalidCredentialsError;
+    if (!areCredentialsValid) throw new UserCredentialsInvalidCredentialsError();
 
-    const secureUser = detailedToSecureUser(user);
-    return secureUser;
+    const detailedSecureUser = detailedUserToDetailedSecureUser(detailedUser);
+    return detailedSecureUser;
   };
 }
 
-export default AuthenticationUsecaseApplication;
+export default AuthenticationUsecase;
